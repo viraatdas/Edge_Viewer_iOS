7453df7bd7c29a55a57b265f164a4a2efe421888
Viraat Das <viraat.laldas@gmail.com>
2019-05-15 23:39:11 -0400

    first commit

---
+++ Edge_Detector/Edge_Detector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Edge_Detector
+//
+//  Created by Viraat Das on 5/15/19.
+//  Copyright © 2019 Viraat Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Edge_Detector/Edge_DetectorTests/Edge_DetectorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Edge_DetectorTests.swift
+//  Edge_DetectorTests
+//
+//  Created by Viraat Das on 5/15/19.
+//  Copyright © 2019 Viraat Das. All rights reserved.
+//
+
+import XCTest
+@testable import Edge_Detector
+
+class Edge_DetectorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
